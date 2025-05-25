0b38ecd086b8b3ffadd5966c2edfecdaf0580b28
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-26 01:58:17 +0530

    sign inpage

---
+++ lib/pages/signin_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:invo/blocs/auth/auth_bloc.dart';
+
+class SigninPage extends StatefulWidget {
+   SigninPage({super.key});
+
+  @override
+  State<SigninPage> createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthBloc>().add(AuthCheckRequested());
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Google Sign-In')),
+      body: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state is AuthFailure) {
+            return Center(child: Text('Error: ${state.error}'));
+          }
+
+          if (state is AuthSuccess) {
+            return Center(child: Text('Welcome ${state.user.email}'));
+          }
+
+          return Center(
+            child: ElevatedButton.icon(
+              icon: const Icon(Icons.login),
+              label: const Text('Sign in with Google'),
+              onPressed: () {
+                context.read<AuthBloc>().add(SignInWithGoogle());
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
