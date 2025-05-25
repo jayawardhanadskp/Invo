fc1c02458ecd689a9aa0a9768a2f31ca3ff02092
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-26 01:53:27 +0530

    app theme

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  
+  static ThemeData get lightTheme {
+    return ThemeData(
+      // brightness: Brightness.light,
+      fontFamily: 'Inter_18pt-Regular',
+      scaffoldBackgroundColor: Colors.grey[50],
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      // brightness: Brightness.dark,
+      fontFamily: 'Inter',
+      scaffoldBackgroundColor: Color(0xFF01031A),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        
+      ),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      
+    );
+  }
+}
