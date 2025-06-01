2b157109c4c6316f56aa2a4a6a93bb8d7019d486
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-02 00:37:43 +0530

    index backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import express from "express";
 import authRoutes from "./routes/auth.routes";
+import batchRoutes from "./routes/batch.routes";
 
 admin.initializeApp();
 const app = express();
@@ -9,5 +10,6 @@ app.use(express.json());
 
 // mount the routes
 app.use("/auth", authRoutes);
+app.use("/batch", batchRoutes)
 
 exports.api = functions.https.onRequest(app);
