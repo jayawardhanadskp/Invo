7244042d721a65e4c796202c35eaa270036a0ca8
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-02 00:45:30 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:invo/blocs/auth/auth_bloc.dart';
+import 'package:invo/blocs/batch/batch_bloc.dart';
 import 'package:invo/firebase_options.dart';
 import 'package:invo/pages/main_page.dart';
 import 'package:invo/pages/signin_page.dart';
 import 'package:invo/repositories/auth_repository.dart';
+import 'package:invo/repositories/batch_repository.dart';
 import 'package:invo/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Hive.initFlutter();
+  await Hive.openBox('settings');
+
   runApp(const MyApp());
 }
 
@@ -24,18 +31,19 @@ class MyApp extends StatelessWidget {
         RepositoryProvider<AuthRepository>(
           create: (context) => AuthRepository(),
         ),
+        RepositoryProvider<BatchRepository>(create: (context) => BatchRepository()),
       ],
       child: MultiBlocProvider(
         providers: [
           BlocProvider<AuthBloc>(create: (_) => AuthBloc(AuthRepository())),
+          BlocProvider<BatchBloc>(create: (_) => BatchBloc(BatchRepository())),
         ],
         child: MaterialApp(
           title: 'Invo',
           theme: AppTheme.darkTheme,
-          home: MainPage()
+          home: SigninPage(),
         ),
       ),
     );
   }
 }
-
