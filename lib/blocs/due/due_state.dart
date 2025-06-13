86a342201fd882fbc22857b690646b66db3c8ac7
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-13 23:31:31 +0530

    due state updated

--- lib/blocs/due/due_state.dart
+++ lib/blocs/due/due_state.dart
@@ -8,14 +8,18 @@ class DueDataState extends DueState {
   final List<dynamic>? dueDetailsList;
   final bool isLoadingCount;
   final bool isLoadingList;
+  final bool isLoadingPayDue;
   final String? error;
+  final String? payDueSucess;
 
   DueDataState({
     this.count,
     this.dueDetailsList,
     this.isLoadingCount = false,
     this.isLoadingList = false,
+    this.isLoadingPayDue = false,
     this.error,
+    this.payDueSucess,
   });
 
   DueDataState copyWith({
@@ -23,14 +27,18 @@ class DueDataState extends DueState {
     List<dynamic>? dueDetailsList,
     bool? isLoadingCount,
     bool? isLoadingList,
+    bool? isLoadingPayDue,
     String? error,
+    String? payDueSucess,
   }) {
     return DueDataState(
       count: count ?? this.count,
       dueDetailsList: dueDetailsList ?? this.dueDetailsList,
       isLoadingCount: isLoadingCount ?? this.isLoadingCount,
       isLoadingList: isLoadingList ?? this.isLoadingList,
+      isLoadingPayDue: isLoadingPayDue ?? this.isLoadingPayDue,
       error: error ?? this.error,
+      payDueSucess: payDueSucess ?? this.payDueSucess,
     );
   }
 }
