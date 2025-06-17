be3630058e31916802892c0ca2299f4e400f4569
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-18 00:17:23 +0530

    batch routes backend

--- functions/src/routes/batch.routes.ts
+++ functions/src/routes/batch.routes.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import {authenticate} from '../middlewares/auth.middleware';
 import { handleCreateBatch } from '../controllers/batch.controller';
+import { getBatchListController } from '../controllers/batch.controller';
 
 const router = Router();
 
 router.post('/create', authenticate, handleCreateBatch);
+router.get('/batchList', authenticate, getBatchListController);
 
 export default router;
