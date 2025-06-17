4da5090c1c51eadbedd84ccbc18346929df0bb6d
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-18 00:17:45 +0530

    due routes backend

--- functions/src/routes/due.routes.ts
+++ functions/src/routes/due.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express"
 import { authenticate } from "../middlewares/auth.middleware"
 import { getAllDuesController } from "../controllers/due.controller"
 import { getBuyersWithDueController } from "../controllers/due.controller"
+import { applyPaymentController } from "../controllers/due.controller"
 
 const router = Router();
 
 router.get("/dueCount", authenticate, getAllDuesController);
 router.get("/dueList", authenticate, getBuyersWithDueController);
+router.post("/payDue", authenticate, applyPaymentController);
 
 export default router;
