392a9789fbf54a69ae4997ba6968a37e49764d86
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-18 00:20:06 +0530

    batch repository

--- lib/repositories/batch_repository.dart
+++ lib/repositories/batch_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:invo/models/batch_model.dart';
+import 'package:invo/models/buyer_model.dart';
 import 'package:invo/services/database_service.dart';
 import 'package:invo/utils/constant.dart';
 
@@ -9,8 +10,7 @@ class BatchRepository {
 
   Future<void> createBatch(
     String grams,
-    String pieces,
-    String quantity,
+    int pieces,
     String note,
     DateTime createAt,
   ) async {
@@ -48,6 +48,36 @@ class BatchRepository {
     } catch (e) {
       print('Error creating batch: $e');
       throw Exception('Failed to create batch: $e');
+    }
+  }
+
+  Future<List<BatchModel>> getBatchList() async {
+    try {
+      final idToken = await DatabaseService().getToken();
+      if (idToken == null) {
+        throw Exception('No ID token found');
+      }
+
+      final response = await _dio.get(
+        '$_baseUrl/batch/batchList',
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $idToken',
+            'Content-Type': 'application/json',
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> rawList = response.data['data'];
+        final List<BatchModel> batchList =
+            rawList.map((batch) => BatchModel.fromMap(batch)).toList();
+        return batchList;
+      } else {
+        throw Exception('Failed to get batch list');
+      }
+    } catch (e) {
+      throw Exception(e);
     }
   }
 }
