3f8333b450fd630868b7340ff6123f7f9260f6d1
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-18 00:16:42 +0530

    recent sale model

---
+++ lib/models/recent_sale_model.dart
@@ -0,0 +1,13 @@
+class RecentSale {
+  final String customerName;
+  final String amount;
+  final DateTime dateTime;
+  final String paymentMethod;
+
+  RecentSale({
+    required this.customerName,
+    required this.amount,
+    required this.dateTime,
+    required this.paymentMethod,
+  });
+}
