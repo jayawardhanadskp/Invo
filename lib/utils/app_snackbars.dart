8b1ed877ffab397587ca34f2a0843eaab8b69896
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-18 00:17:02 +0530

    app snackbars

--- lib/utils/app_snackbars.dart
+++ lib/utils/app_snackbars.dart
@@ -5,6 +5,6 @@ class AppSnackbars {
   static void showSucessSnackbar(BuildContext context, String message) {
     ScaffoldMessenger.of(
       context,
-    ).showSnackBar(SnackBar(content: Center(child: Text(message))));
+    ).showSnackBar(SnackBar(content: Center(child: Text(message)), backgroundColor: const Color.fromARGB(255, 147, 211, 150),));
   }
 }
