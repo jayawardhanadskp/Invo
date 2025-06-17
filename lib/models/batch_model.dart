656776912a847cef438f2347f2200f2e9b9eace8
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-18 00:20:00 +0530

    batch model

--- lib/models/batch_model.dart
+++ lib/models/batch_model.dart
@@ -1,19 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 class BatchModel {
   final String? id;
   final String grams;
-  final String pieces;
+  final int pieces;
   final String? note;
   final DateTime createdAt;
+  final int? sales;
   BatchModel({
     this.id,
     required this.grams,
     required this.pieces,
     this.note,
     required this.createdAt,
+    this.sales,
   });
-
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
@@ -22,20 +24,23 @@ class BatchModel {
       'pieces': pieces,
       'note': note,
       'createdAt': createdAt.millisecondsSinceEpoch,
+      'sales': sales,
     };
   }
 
   factory BatchModel.fromMap(Map<String, dynamic> map) {
     return BatchModel(
-      id: map['id'] as String,
+      id: map['id'] != null ? map['id'] as String : null,
       grams: map['grams'] as String,
-      pieces: map['pieces'] as String,
+      pieces: map['pieces'] as int,
       note: map['note'] != null ? map['note'] as String : null,
-      createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt'] as int),
+      createdAt: DateTime.parse(map['createdAt'] as String),
+      sales: map['sales'] != null ? map['sales'] as int : null,
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory BatchModel.fromJson(String source) => BatchModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory BatchModel.fromJson(String source) =>
+      BatchModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
