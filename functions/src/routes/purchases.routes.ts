8fa5d5ce64432824a9445909e49bf5dfa6823ee4
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-06-18 00:18:09 +0530

    purchase routes backend

--- functions/src/routes/purchases.routes.ts
+++ functions/src/routes/purchases.routes.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import {authenticate} from "../middlewares/auth.middleware";
 import {createPurchaseController} from "../controllers/purchases.controller";
+import {getPurchaseListController} from "../controllers/purchases.controller";
 
 const router = Router();
 
 router.post("/create", authenticate, createPurchaseController);
+router.get('/purchaseList', authenticate, getPurchaseListController);
 
 export default router;
