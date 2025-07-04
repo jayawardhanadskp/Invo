7a0c25db93e7171d1b207fbc11b8eec99939f262
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-07-04 23:26:33 +0530

    fingureprint screen and main (added bloc)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:invo/blocs/auth/auth_bloc.dart';
 import 'package:invo/blocs/batch/batch_bloc.dart';
+import 'package:invo/blocs/biometric/biometric_bloc.dart';
 import 'package:invo/blocs/buyer/buyer_bloc.dart';
 import 'package:invo/blocs/due/due_bloc.dart';
 import 'package:invo/blocs/purchase/purchase_bloc.dart';
@@ -13,6 +14,7 @@ import 'package:invo/pages/fingureprint_screen.dart';
 import 'package:invo/pages/splash_page.dart';
 import 'package:invo/repositories/auth_repository.dart';
 import 'package:invo/repositories/batch_repository.dart';
+import 'package:invo/repositories/biometric_repository.dart';
 import 'package:invo/repositories/buyer_repository.dart';
 import 'package:invo/repositories/due_repository.dart';
 import 'package:invo/repositories/purchases_repository.dart';
@@ -43,7 +45,8 @@ class MyApp extends StatelessWidget {
         RepositoryProvider<PurchasesRepository>(create: (context) => PurchasesRepository()),
         RepositoryProvider<BuyerRepository>(create: (context) => BuyerRepository()),
         RepositoryProvider<DueRepository>(create: (context) => DueRepository()),
-        RepositoryProvider<WhatsappMessageRepository>(create: (context) => WhatsappMessageRepository())
+        RepositoryProvider<WhatsappMessageRepository>(create: (context) => WhatsappMessageRepository()),
+        RepositoryProvider<BiometricRepository>(create: (context) => BiometricRepository())
       ],
       child: MultiBlocProvider(
         providers: [
@@ -53,6 +56,7 @@ class MyApp extends StatelessWidget {
           BlocProvider<BuyerBloc>(create: (_) => BuyerBloc(BuyerRepository())),
           BlocProvider<DueBloc>(create: (_) => DueBloc(DueRepository())),
           BlocProvider<WhatsappBloc>(create: (_) => WhatsappBloc(WhatsappMessageRepository())),
+          BlocProvider<BiometricBloc>(create: (_) => BiometricBloc(BiometricRepository()))
         ],
         child: MaterialApp(
           title: 'Invo',
--- lib/pages/fingureprint_screen.dart
+++ lib/pages/fingureprint_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:invo/blocs/biometric/biometric_bloc.dart';
+import 'package:invo/pages/main_page.dart';
 
 class FingureprintScreen extends StatefulWidget {
   const FingureprintScreen({super.key});
@@ -11,108 +16,148 @@ class FingureprintScreen extends StatefulWidget {
 
 class _FingureprintScreenState extends State<FingureprintScreen> {
   @override
+  void initState() {
+    super.initState();
+    context.read<BiometricBloc>().add(AuthenticateBiometric());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromARGB(255, 29, 33, 71),
-              Color.fromARGB(255, 6, 9, 40),
-              Color(0xFF01031A),
-            ],
-            stops: [0.0, 0.6, 1.0],
+      body: BlocListener<BiometricBloc, BiometricState>(
+        listener: (context, state) {
+          if (state is BiometricSuccess) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Authentication Successful!')),
+            );
+            Timer(Duration(milliseconds: 1500), () {
+              Navigator.of(context).pushReplacement(
+                PageRouteBuilder(
+                  pageBuilder:
+                      (context, animation, secondaryAnimation) => MainPage(),
+                  transitionsBuilder: (
+                    context,
+                    animation,
+                    secondaryAnimation,
+                    child,
+                  ) {
+                    return FadeTransition(opacity: animation, child: child);
+                  },
+                  transitionDuration: Duration(milliseconds: 500),
+                ),
+              );
+            });
+          } else if (state is BiometricFailure) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          } else if (state is BiometricUnavailable) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 29, 33, 71),
+                Color.fromARGB(255, 6, 9, 40),
+                Color(0xFF01031A),
+              ],
+              stops: [0.0, 0.6, 1.0],
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              children: [
-                const SizedBox(height: 100),
-                Text(
-                  ' Unlock Your Account',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-
-                Text(
-                  'Use your fingerprint or enter your password to continue.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            const SizedBox(height: 70),
-            Column(
-              children: [
-                SvgPicture.asset(
-                  'assets/svg/fingureprint.svg',
-                  color: Colors.white,
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  'Tap to unlock with fingerprint',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 70),
-            Column(
-              children: [
-                Center(
-                  child: ElevatedButton.icon(
-                    icon: SvgPicture.asset('assets/svg/googleg.svg'),
-                    label: const Text(
-                      'Sign in with Google',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(50, 101, 82, 193),
-                      padding: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 22,
-                      ),
-                      fixedSize: Size(220, 45),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: Text(
-                    'We use your Google account to keep things secure—no passwords needed.',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 100),
+                  Text(
+                    ' Unlock Your Account',
                     style: TextStyle(
                       color: Colors.white,
-                      fontWeight: FontWeight.w200,
-                      fontSize: 11,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Use your fingerprint or enter your password to continue.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
                     ),
                     textAlign: TextAlign.center,
                   ),
-                ),
-                const SizedBox(height: 30),
-              ],
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(height: 70),
+              Column(
+                children: [
+                  SvgPicture.asset(
+                    'assets/svg/fingureprint.svg',
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    'Tap to unlock with fingerprint',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 70),
+              Column(
+                children: [
+                  Center(
+                    child: ElevatedButton.icon(
+                      icon: SvgPicture.asset('assets/svg/googleg.svg'),
+                      label: const Text(
+                        'Sign in with Google',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(50, 101, 82, 193),
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 22,
+                        ),
+                        fixedSize: Size(220, 45),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Text(
+                      'We use your Google account to keep things secure—no passwords needed.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w200,
+                        fontSize: 11,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
